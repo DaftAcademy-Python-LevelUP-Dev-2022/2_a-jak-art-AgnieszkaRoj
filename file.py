ce65266719932ea908e4ffe4386959dc7e847046
AgnieszkaRoj <105599478+AgnieszkaRoj@users.noreply.github.com>
2022-05-18 22:14:15 +0200

    Update file.py

--- file.py
+++ file.py
@@ -7,29 +7,7 @@ def greeter(func):
 
 def sums_of_str_elements_are_equal(func):
     pass
-   """ def inner_num(*args, **kwargs):
-        nonlocal negative
-        numbers = func(*args, **kwargs).split()
-        numbers = [int(x) for x in numbers]
-        if digitSum(numbers[0]) == digitSum(numbers[1]):
-            info = f'{digitSum(numbers[0]) == {digitSum(numbers[1])}'
-        else:
-            info = f'{digitSum(numbers[0]) != {digitSum(numbers[1])}'
-        return info
-    return inner_num
-    def digitSum(n):
-        digits = []
-        minus = False
-        for digit in str(n):
-            if digit == '-':
-                minus = True
-            if minus:
-                liczba = -int(digit)
-                digits.append(liczba)
-            else:
-                liczba = int(digit)
-                digits.append(liczba)
-        return sum(digits) """
+ 
 
 
 def format_output(*required_keys):
